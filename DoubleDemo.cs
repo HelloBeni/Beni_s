1eaa86ced3cead46a598a9cdb3c61f48cbebbc0a
HelloBeni <113401893+HelloBeni@users.noreply.github.com>
2024-10-16 10:43:07 +0900

    Add files via upload

---
+++ DoubleDemo.cs
@@ -0,0 +1,18 @@
+//DoubleDemo.cs
+//
+
+
+using System;
+
+class DoubleDemo
+{
+    static void Main()
+    {
+        double PI = 3.141592;
+        Console.WriteLine("{0}", PI);
+    }
+}//
+
+
+//실행 결과 :
+//3.141592
